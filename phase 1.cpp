ec86a577ffa159598c98ffbd411f02f242efd2be
sneha1012 <snehamaurya1012@gmail.com>
2023-03-07 17:59:17 +0530

    arrays

--- phase 1.cpp
+++ phase 1.cpp
@@ -1,34 +1,26 @@
+
 #include <iostream>
 using namespace std;
 
+int main() {
 
-int main()
-{
-  int amount;
-  cin>>amount;
-  int Rs100 , Rs50 , Rs20 , Rs1;
+  int numbers[5] = {7, 5, 6, 12, 35};
 
-  switch(1)
-  {
-      case 1: Rs100=amount/100;
-              amount=amount%100;
-              cout<<"No of 100 Rupee ="<<Rs100<<endl;
-              
-         
-      case 2: Rs50=amount/50;
-              amount=amount%50;
-              cout<<"No of 50 Rupee ="<<Rs50<<endl;
-              
+  cout << "The numbers are: ";
 
-      case 3: Rs20=amount/20;
-              amount=amount%20;
-              cout<<"No of 20 Rupee ="<<Rs20<<endl;
-               
+  //  Printing array elements
+  // using range based for loop
+  for (const int &n : numbers) {
+    cout << n << "  ";
+  }
 
-      case 4: Rs1=amount/1;
-              amount=amount%1;
-              cout<<"No of 1 Rupee ="<<Rs1<<endl; 
-          
-           }
-        return 0;
+  cout << "\nThe numbers are: ";
+
+  //  Printing array elements
+  // using traditional for loop
+  for (int i = 0; i < 5; ++i) {
+    cout << numbers[i] << "  ";
+  }
+
+  return 0;
 }
