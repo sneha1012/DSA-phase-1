91c6ecf6b1845139f4f48ed84566b9b8023a1990
sneha1012 <snehamaurya1012@gmail.com>
2023-03-12 17:52:36 +0530

    arrayinto

---
+++ declaration arryas.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void printarray(){
+
+}
+int main(){
+
+    //declare
+    int number[15];
+
+    //accessing an array
+    cout<<"value at 14 index"<< number[14]<< endl;
+
+
+    int second[3] = {5, 7, 11};
+
+    cout<< "value at 2 index" << second[2] <<endl;
+
+    cout<< endl<< " everything is fine" << endl<< endl;
+
+
+    return 0;
+}
