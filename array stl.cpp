06a08260e25fae0c66a6b93449e2d7b2ca533569
sneha1012 <snehamaurya1012@gmail.com>
2023-03-21 10:48:21 +0530

    initial commit

---
+++ array stl.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<array>
+
+using namespace std;
+int main(){
+
+    int basic[3] = {1,2,3};
+
+    array<int, 4> a ={ 1, 2, 3, 4};
+
+    int size= a.size();
+
+    for(int i=0; i<size; i++){
+        cout<<a[i]<<endl;
+    }
+     
+
+     cout<<"element first" << a.front()<< endl;
+     cout<<"last element"<< a.back()<<endl;
+    }
+
