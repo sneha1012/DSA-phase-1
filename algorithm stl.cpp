74375fb2ea20f36fee75f11a387bc7e2d3ce222c
sneha1012 <snehamaurya1012@gmail.com>
2023-03-24 19:15:09 +0530

    initial commit

---
+++ algorithm stl.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+int main(){
+
+    vector<int> v;
+
+    v.push_back(1);
+    v.push_back(3);
+    v.push_back(6);
+    v.push_back(7);
+
+    cout<<"finding 6 in the data"<<binary_search(v.begin(), v.end() , 6 )<<endl;
+    cout<<"lower bound"<<lower_bound(v.begin(), v.end(), 6)-v.begin()<<endl;
+    cout<<"upper bound"<<upper_bound(v.begin(), v.end(), 4)-v.begin()<<endl;
+
+}
