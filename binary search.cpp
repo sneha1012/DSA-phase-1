3f6c6e66d8760873958a106fab0443c7ede14928
sneha1012 <snehamaurya1012@gmail.com>
2023-04-14 22:52:52 +0530

    initial commit

--- binary search.cpp
+++ binary search.cpp
@@ -1,48 +1,32 @@
 #include<iostream>
+
 using namespace std;
+int getPivot(int arr[], int n){
+    int s=0;
+    int e= n-1;
+    int mid= s + (e-s)/2;
 
-int binarySearch(int arr[], int size, int key){
-
-    int start= 0;
-    int end = size-1;
-
-    int mid= start + (end-start)/2;
-    while(start <= end){
-        if(arr[mid]== key){
-            return mid;
+    while(s<e){
+        if(arr[mid] >= arr[0])
+        {
+            s= mid +1;
+        } else{
+            e= mid;
         }
-        // got to right if the key is greater than the mid
-        if(key > arr[mid]){
-            start= mid + 1;
-        }
-        else{
-            end = mid - 1;
-        }
-        mid = start + (end-start)/2; // we need to  update mid acc to end and start respectively
-
+        mid= s+ (e-s)/2;
     }
-    return -1;
+    return s; // pivot element will be the same return s or e doesn't matter
+
 }
+int main() {
 
-int main()
-{
-    int even[6]= {2,4,6,8,12,18};
-    int odd[5]= {3,8, 11, 14, 16};
-
-    int evenindex = binarySearch(even, 6, 12 );
-
-    cout<<"index of 12 is"<< evenindex<< endl;
-     int oddindex = binarySearch(odd, 5, 20 );
-     cout<<"index of 20 is"<< oddindex<< endl;
-
-
-
-
-
-
-
-
-    return 0;
+    int arr[5]= {8, 10, 17, 1, 3};
+    cout<< "pivot is"<< getPivot(arr, 5) << endl;
 }
 
 
+
+
+// we can apply binary search on monotonic functions 
+// n iss your size of array
+// (n-1) will be the index of the last element
